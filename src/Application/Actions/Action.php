0740a6cf754ea9895968c10a786141bcfac1967d
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2021-01-14 21:24:31 +0000

    Fix static analysis errors

--- src/Application/Actions/Action.php
+++ src/Application/Actions/Action.php
@@ -74,7 +74,7 @@ abstract class Action
      */
     protected function getFormData()
     {
-        $input = json_decode(file_get_contents('php://input'));
+        $input = json_decode((string) file_get_contents('php://input'), true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new HttpBadRequestException($this->request, 'Malformed JSON input.');
@@ -114,7 +114,7 @@ abstract class Action
      */
     protected function respond(ActionPayload $payload): Response
     {
-        $json = json_encode($payload, JSON_PRETTY_PRINT);
+        $json = (string) json_encode($payload, JSON_PRETTY_PRINT);
         $this->response->getBody()->write($json);
 
         return $this->response
