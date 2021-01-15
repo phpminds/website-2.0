38bd6d13747b5b935973db7b6df37c09d3604ae4
balhayre <baljinder.hayre@live.co.uk>
2021-01-15 21:43:23 +0000

    Updated Nav-bar with alt text and added social icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-
       alignItems: ['hover', 'focus'],
       transformOrigin: {
-                '24': '6rem',
-               'full': '100%',
-      }
-    },
-    colors: {
-      ...colors,
-      'bg-green': '#6AA84F'
+        '24': '6rem',
+        'full': '100%',
+      },
+      fontSize: {'big':'3rem'},
+      colors: {
+        ...colors,
+        'bg-green': '#6AA84F'
+      },
+      fontFamily: {
+        'sofia': ['Sofia']
+      },
     }
   },
-  variants: {
-    extend: {},
-  },
+
+  variants: {},
   plugins: [],
-
-
 }
